6177c701c80b7834f492979bb1c4a745c81087c5
mothmelons <alexkulll001@gmail.com>
2025-05-30 15:15:32 +0300

    Minor changes

---
+++ GrandFinale/scripts.js
@@ -0,0 +1,6 @@
+function searchItems(event) {
+    event.preventDefault()
+    let query = document.querySelector('#searchForm input').value.toLowerCase()
+    let itemList = document.querySelector('.item-list')
+    
+}
