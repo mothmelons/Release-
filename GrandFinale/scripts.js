865bd7cefc8107ffeb98c9f1792b21efc413cece
mothmelons <alexkulll001@gmail.com>
2025-06-01 19:38:28 +0300

    Revamp + Last (or second to last commit)

--- GrandFinale/scripts.js
+++ GrandFinale/scripts.js
@@ -1,6 +1,7 @@
-function searchItems(event) {
-    event.preventDefault()
-    let query = document.querySelector('#searchForm input').value.toLowerCase()
-    let itemList = document.querySelector('.item-list')
-    
+
+function changeVideo(url) {
+  const player = document.getElementById('videoPlayer');
+  player.src = url;
+  player.load();
+  player.play();
 }
